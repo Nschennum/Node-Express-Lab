d66b19a2e8fe94c0fe9c20ce5d2961d4d7c7024f
Natalie Schennum <natalieschennum@gmail.com>
2018-05-02 12:00:47 -0700

    pulled from master

--- server.js
+++ server.js
@@ -1,5 +1,63 @@
-// import your node modules
+const express = require('express');
 
-const db = require('./data/db.js');
+// import db from './data/db';
+const db = require('./data/db');
 
-// add your server code starting here
+const server = express();
+
+server.get('/', (req, res) => {
+  res.send('Api running');
+});
+
+server.get('/api/users', (req, res) => {
+  //get the users
+  db
+    .find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+      // do something with the error
+    });
+});
+
+// /api/users/123
+server.get('/api/users/:id', (req, res) => {
+  // grab the id from URL parameters
+  const id = req.params.id;
+
+  db
+    .findById(id)
+    .then(users => {
+      if (users.length === 0) {
+        res.status(404).json({ message: 'user not found' });
+      } else {
+        res.json(users[0]);
+      }
+    })
+    .catch(err => {
+      // do something with the error
+      res.status(500).json({ error: err });
+    });
+});
+
+server.post('/api/users', (req, res) => {
+    const userInformaiton = req.body; 
+    console.log('user informaiton', userInformation);
+
+    db
+    .insert(userInformation)
+    .then(response => {
+        res.status(201).json(response); 
+    })
+    .catch(err =>{ 
+        res.status(500).json({err: err}); 
+
+    });
+});
+
+server.get('/api/posts', (req, res) => {
+
+});
+server.listen(5000, () => console.log('\n== API Running on port 5000 ==\n'));
